6a9896ec9c9e54251bc8e488d316d5bc21503ba8
abeera346 <abeeraabdulmajeed@gmail.com>
2025-03-01 12:10:46 +0400

    Coding is cool

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+#Assinging value to variable -1
+
+Word1= "coding"
+Word2= "is"
+Word3= "cool"
+
+#Use string concatenation to combine the three words and print the phrase.
+
+print(Word1 + " " + Word2 + " " + Word3)
