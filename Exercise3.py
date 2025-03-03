c79456e97a0fa27b9dba04dbb5bf6fe44e96d67a
abeera346 <abeeraabdulmajeed@gmail.com>
2025-03-03 14:48:08 +0400

    Print name, hometown, age.

---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+#Program stores and prints the name, hometown and age.
+
+Name= ("Abeera abdul majeed")
+Hometown= ("Lahore")
+Age= ("Nineteen")
+
+#Printing the name, hometown and age on separate lines using single 'print()' statement.
+
+print("Name: ", Name, "\nHometown:", Hometown, "\nAge:", Age)
+
